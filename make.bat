f327d8de37b9cd16000e628ff758fe7c49265969
SunlitSpace542 <35207254+Sunlitspace542@users.noreply.github.com>
2026-04-19 13:56:32 -0400

    Update make.bat

--- make.bat
+++ make.bat
@@ -4,4 +4,5 @@ if exist SGSOUND0.BIN del SGSOUND0.BIN
 
 asar --symbols=nocash SGSOUND0.asm SGSOUND0.BIN
 
-@fc /B SGSOUND0.BAK SGSOUND0.BIN | head -n5
+::@fc /B SGSOUND0.BAK SGSOUND0.BIN
+:: | head -n5
